ec5a039e4b81a09128421d7a6e76d5ccb70bbab1
Wesley Smith <wesjavadev@gmail.com>
2016-04-09 17:34:03 -0400

    Whoopsies

--- src/main/java/me/wes/worldlib/WorldLib.java
+++ src/main/java/me/wes/worldlib/WorldLib.java
@@ -27,17 +27,6 @@ package me.wes.worldlib;
 import org.apache.commons.lang.Validate;
 import org.bukkit.plugin.java.JavaPlugin;
 
-/**
- * ******************************************************************************************************
- * Copyright (C) 2015 Wesley Smith under the Copyright Law of the United States. All rights reserved.   *
- * All code contained in this document is sole property of Wesley Smith. Do NOT distribute, reproduce,  *
- * take snippets, or take this code under your name without exclusive permission from Wesley Smith.     *
- * Not following this statement will result in a void of all agreements made, and possibly legal action.*
- * If you have any questions or concerns, please contact me via email with the address of               *
- * wesjavadev@gmail.com                                                                                 *
- * Thanks for your cooperation.                                                                         *
- * ******************************************************************************************************
- */
 public class WorldLib {
 
     private static WorldLib instance = new WorldLib();
